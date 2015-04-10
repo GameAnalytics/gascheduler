801a8ef5147bedf62a9f5213c3c6b529bac1b628
Chris De Vries <chris.de-vries@gameanalytics.com>
2015-04-10 16:37:40 +0200

    Clean up tests.

--- test/gascheduler_test.erl
+++ test/gascheduler_test.erl
@@ -12,12 +12,55 @@ gascheduler_test_() ->
       {timeout, 10, ?_test(execute_tasks())}
     ]}.
 
+-define(MASTER, master@localhost).
+
 %%
 %% Setup
+%%
 
-setup() -> ok.
-teardown(_) -> ok.
+setup() ->
+    _ = os:cmd("epmd -d"),
+    {ok, _Master} = net_kernel:start([?MASTER, shortnames]),
+    ok.
 
+setup_slaves(Num) ->
+    Slaves = [ Slave || {ok, Slave} 
+                <- [ slave:start_link(localhost, "slave" ++ integer_to_list(N))
+                     || N <- lists:seq(1, Num) ] ],
+    ?assertEqual(length(Slaves), Num),
+    [?MASTER | Slaves].
+
+teardown(_) ->
+    _ = net_kernel:stop(),
+    ok.
+
+%%
+%% Utilities
+%%
+
+test_jobs(N, Nodes) ->
+    Jobs = lists:seq(1, N),
+    ok = lists:foreach(
+        fun(Id) ->
+            ok = gascheduler:execute({gascheduler_test, do_some_work, [Id]})
+        end, Jobs), 
+    Received = lists:map(
+        fun(_) ->
+            receive
+                {{ok, Id}, Node, {Mod, Fun, Args}} ->
+                    ?assertEqual(gascheduler_test, Mod), 
+                    ?assertEqual(do_some_work, Fun),
+                    ?assertEqual(length(Args), 1),
+                    ?assertEqual(hd(Args), Id),
+                    {Id, Node};
+                _ ->
+                    ?assert(false),
+                    {-1, no_node}
+            end     
+         end, Jobs), 
+    {ReceivedIds, ReceivedNodes} = lists:unzip(Received),     
+    ?assertEqual(lists:usort(Nodes), lists:usort(ReceivedNodes)),
+    ?assertEqual(lists:sort(ReceivedIds), Jobs).   
 
 %%
 %% Tests
@@ -29,41 +72,17 @@ teardown(_) -> ok.
 %% assert that work is finished
 %% assert all stats are updated
 execute_tasks() ->
-    MasterNode = master@localhost,
-    {ok, Master} = net_kernel:start([MasterNode, shortnames]),
-    {ok, SlaveNode} = slave:start_link(localhost, slave1), 
-    ?assertEqual([slave1@localhost], nodes()),
-    Nodes = [MasterNode | nodes()],
+    Nodes = [MasterNode, SlaveNode] = setup_slaves(1),
     MaxWorkers = 10,
     MaxRetries = 10,
     Client = self(),
-    {ok, Scheduler} = gascheduler:start_link(Nodes, Client, MaxWorkers, MaxRetries),
+    {ok, _} = gascheduler:start_link(Nodes, Client, MaxWorkers, MaxRetries),
 
-    Jobs = lists:seq(1, 25),
-    ok = lists:foreach(
-        fun(Id) ->
-            ok = gascheduler:execute({gascheduler_test, do_some_work, [Id]})
-        end, Jobs), 
-    Received = lists:map(
-        fun(_) ->
-            receive
-                Msg = {{ok, Id}, Node, {Mod, Fun, Args}} ->
-                    ?assertEqual(gascheduler_test, Mod), 
-                    ?assertEqual(do_some_work, Fun),
-                    ?assertEqual(length(Args), 1),
-                    ?assertEqual(hd(Args), Id),
-                    {Id, Node};
-                Msg ->
-                    ?assert(false),
-                    {-1, no_node}
-            end     
-         end, Jobs), 
-    {ReceivedIds, ReceivedNodes} = lists:unzip(Received),     
-    ?assertEqual(lists:usort(Nodes), lists:usort(ReceivedNodes)),
-    ?assertEqual(lists:sort(ReceivedIds), Jobs),     
+    NumJobs = 25,
+    test_jobs(NumJobs, Nodes),
     
     Stats = gascheduler:stats(),
-    ?assertEqual(proplists:get_value(ticks, Stats), length(Jobs)),
+    ?assertEqual(proplists:get_value(ticks, Stats), NumJobs),
     ?assertEqual(proplists:get_value(pending, Stats), 0),
     ?assertEqual(proplists:get_value(running, Stats), 0),
     ?assertEqual(proplists:get_value(max_workers, Stats), MaxWorkers),
@@ -72,9 +91,9 @@ execute_tasks() ->
     ?assertEqual(proplists:get_value(worker_nodes, Stats), [{MasterNode, 0},
                                                             {SlaveNode, 0}]),
 
-    _ = net_kernel:stop(),
     ok.
 
 do_some_work(Id) ->
    timer:sleep(100),
    Id.
+
